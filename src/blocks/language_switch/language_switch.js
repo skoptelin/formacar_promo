e3de81ef80658e1a536633e67d2940dabea89cb5
Stas Koptelin <stan.koptelin@yandex.ru>
2024-02-01 13:52:36 +0300

    added Arabic language

--- src/blocks/language_switch/language_switch.js
+++ src/blocks/language_switch/language_switch.js
@@ -1,5 +1,87 @@
 (() => {
 
+    let arr_lang = {
+        'en': {
+            'formacar_design': 'Formacar Design is a studio dedicated to premium vehicle design and styling.',
+            'formacar_description': 'The high standards and the individual approach adopted by Formacar offer a fresh perspective on the mundane and let you discover new aspects of automotive design finesse.',
+            'our_works': 'Our works',
+            'for_business_partners': 'for business partners',
+            'description_virus2': 'Elegant and assertive at the same time, VIRUS 2 represents a new direction in the design of Porsche 911. Life without emotions and diversity loses its sincerity. If everyone were the same, the world would be less interesting. Individuality matters in every detail, and VIRUS 2 emphasizes your unique style.',
+            'consists_virus2': 'VIRUS 2 consists of 30 advanced carbon fiber elements that will add individuality to your Porsche 911. The exclusive VIRUS body kit for the Porsche 911 992, made of carbon fiber using aviation-grade technology, possesses excellent strength, can withstand temperatures above 120°C, and weighs less than factory counterparts.',
+            'about_formacar': 'FORMACAR HAS CRAFTED ONE OF THE MOST EXTENSIVE AND TECHNOLOGICALLY INTRICATE RESTYLING KITS FOR THE LAMBORGHINI URUS',
+            'aerodynamic_package': 'This aerodynamic package consists of 42 pieces. Among the primary components are the rear bumper with a diffuser, the front bumper, a hood, and a comprehensive expansion that  incorporates the replacement of the front fenders.',
+            'formacar_clients': 'Clients can choose between a kit fabricated from ultra-high-temperature fiberglass (Fiberglass) and an exclusive version made of carbon fiber featuring a visible weave pattern (Visible Carbon).',
+            'aerodynamics_components': 'Components manufactured from ultra-high-temperature fiberglass match the weight of original parts, boast a thickness of 2-3 mm, and can endure temperatures up to 120°C, all while maintaining robust durability.',
+            'aerodynamics_parts': "Parts made from carbon fiber are produced using methods akin to aviation technologies. They offer superior strength, can withstand temperatures beyond 120°C, and are lighter compared to the original components. Additionally, the carbon fiber lends the Italian SUV an aesthetic appeal that's crucial for luxury vehicles.",
+            'mercedes_diamant': 'FORMACAR HAS EXPANDED ITS COLLECTION WITH THE DIAMANT GT PROJECT BASED ON THE Mercedes-AMG GT 63 S MODEL (THE RESTYLING IS COMPATIBLE WITH VERSIONS 3.0, 4.0, 53, 63, AND 63 S)',
+            'formacar_diamant': 'The complete restyling kit includes 40 components. A distinctive feature is the diffuser with a centrally located exhaust tip in the style of the AMG GT Coupe, as well as a double spoiler on the trunk lid for the 63 S version. The hood and front bumper give the car an added sense of aggression and character.',
+            'aerodynamics_diamant': 'Customers can choose an aerodynamic kit made from ultra-high-temperature fiberglass or an exclusive version made of carbon fiber with a unique design. The fiberglass components have a weight equivalent to the original parts and a thickness of 2-3 mm, and are able to withstand temperatures up to 120°C. The exclusive carbon fiber parts are manufactured using techniques akin to aviation standards and boast higher durability, a temperature threshold above 120°C, and a lighter weight compared to the original components.',
+            'become_partner': 'Become a Partner',
+            'with_formacar': 'with Formacar Design!',
+            'about_formacar_design': 'Formacar Design is the hub for crafting automotive designs and styling. Our in-house production and precise oversight at every stage (from creating concepts and prototypes to developing and assembling kits) ensure exceptionally high quality in our projects.',
+            'gcc_partners': "We're expanding our presence in GCC region and actively seeking partners and like-minded individuals to propel our business forward.",
+            'join_our_team': "If you've dreamt of launching and growing a successful venture in the automotive industry, join our team!",
+            'formacar_changes': 'Formacar. It changes everything.',
+            'formacar_catalog': 'Catalog',
+            'formacar_invest_desk': 'Invest Desk',
+            'formacar_get_details': 'Get Details',
+            'formacar_name': 'Name',
+            'formacar_purpose_inquiry': 'Purpose of Inquiry',
+            'business_partnership': 'Business/Partnership',
+            'formacar_investments': 'Investments',
+            'formacar_phone': 'Phone',
+            'formacar_email': 'E-mail',
+            'formacar_message': 'Message',
+            'formacar_send': 'Send',
+            'formacar_captcha': 'This site is protected by reCAPTCHA and the Google Privacy Policy and Terms of Service apply.',
+            'site_captcha': 'This site is protected by reCAPTCHA and the Google',
+            'and': 'and',
+            'privacy_policy': 'Privacy Policy',
+            'terms_service': 'Terms of Service',
+            'button_apply': 'apply',
+            'application_sent': 'Your application has been sent. We will contact you shortly.'
+        },
+        'ar': {
+            'formacar_design': 'هو استوديو متميز لتصميم  السيارات الفاخرة Formacar Design',
+            'formacar_description': 'تسمح المعايير العالية والنهج المميز للعلامة التجارية Formacar  بالنظر إلى المألوف بشكل مختلف وفتح جوانب جديدة للتميز في تصميم السيارات.',
+            'our_works': 'أعمالنا',
+            'for_business_partners': 'لشركاء الأعمال',
+            'description_virus2': 'يمثل VIRUS 2، الأنيق والمُقوَّى في الوقت نفسه، اتجاهًا جديدًا في تصميم Porsche 911. فالحياة الخالية من العاطفة والتنوع تفقد الصدق. لو كان الجميع متشابهين، لكان العالم أقل إثارة للاهتمام. الفردية مهمة في كل التفاصيل، وسوف يسلط VIRUS 2 الضوء على أسلوبكم الفريد.',
+            'consists_virus2': 'يتكون VIRUS 2 من 30 عنصرًا كربونيًا مُطوَّراً ستضيف لمسة شخصية إلى Porsche 911. وتتمتع المجموعة الهيكلية VIRUS الحصرية لـ Porsche 911 992، المصنوعة من ألياف الكربون باستخدام تكنولوجيا الطائرات، بقوة ممتازة، ويمكنها تحمل درجات حرارة أعلى من 120 درجة مئوية، كما أنها تزن أقل من نظيراتها في المصنع.',
+            'about_formacar': 'لقد قمنا بتطوير واحدة من مجموعات إعادة التصميم الأكثر شمولاً وتعقيدًا من الناحية التكنولوجية لـ Lamborghini Urus.',
+            'aerodynamic_package': 'تتكون هذه المجموعة الديناميكية الهوائية من 42 عنصرًا. تشمل المكونات الرئيسية مصدًا خلفيًا مع ناشر هواء، ومصدًا أماميًا، وغطاء محرك السيارة، وامتدادًا شاملاً يشتمل على أجنحة أمامية بديلة.',
+            'formacar_clients': 'يمكن للعملاء الاختيار بين مجموعة مصنوعة من الألياف الزجاجية المقاومة لدرجات الحرارة المرتفعة جداً (Fiberglass)، ونسخة حصرية مصنوعة من ألياف الكربون بنمط نسج مرئي (Visible Carbon). وتتناسب المكونات المصنوعة من الألياف الزجاجية المقاومة لدرجات الحرارة المرتفعة جداً مع وزن الأجزاء الأصلية، ويبلغ سمكها 2-3 ملم ويمكنها تحمل درجات حرارة تصل إلى 120 درجة مئوية مع الحفاظ على خصائصها ومتانتها.',
+            'aerodynamics_components': 'وتتناسب المكونات المصنوعة من الألياف الزجاجية المقاومة لدرجات الحرارة المرتفعة جداً مع وزن الأجزاء الأصلية، ويبلغ سمكها 2-3 ملم ويمكنها تحمل درجات حرارة تصل إلى 120 درجة مئوية مع الحفاظ على خصائصها ومتانتها.',
+            'aerodynamics_parts': "ويتم إنتاج أجزاء من ألياف الكربون باستخدام أساليب مشابهة لتكنولوجيا الطائرات. إنها توفر قوة فائقة، وتتحمل درجات حرارة أعلى من 120 درجة مئوية، كما أنها أخف وزنًا من المكونات الأصلية. بالإضافة إلى ذلك، تمنح ألياف الكربون سيارة الدفع الرباعي الإيطالية جاذبية جمالية تعتبر ضرورية للسيارات الفاخرة.",
+            'mercedes_diamant': 'قامت Formacar بتوسيع مجموعتها من خلال مشروع Diamant على أساس موديل Mercedes-AMG GT 63 S. وتتوافق عملية إعادة التصميم مع الإصدارات 3.0, 4.0, 53, 63  و63 S.',
+            'formacar_diamant': 'تتضمن المجموعة الكاملة 40 عنصرًا. ومن السمات المميزة ناشر الهواء المزود بأنبوب عادم ذو موقع مركزي على طراز AMG GT ، بالإضافة إلى المفسد المزدوج الموجود على غطاء صندوق السيارة على طراز 63 S. يمنح غطاء المحرك والمصد الأمامي السيارة المزيد من العدوانية والشخصية.',
+            'aerodynamics_diamant': 'يمكن للمشترين الاختيار من بين مجموعة أدوات ديناميكية هوائية مصنوعة من الألياف الزجاجية المقاومة لدرجات الحرارة المرتفعة جداً أو نسخة حصرية من ألياف الكربون بتصميم فريد. مكونات الألياف الزجاجية تعادل في الوزن الأجزاء الأصلية، ويبلغ سمكها 2-3 ملم ويمكنها تحمل درجات حرارة تصل إلى 120 درجة مئوية. يتم تصنيع أجزاء ألياف الكربون الحصرية باستخدام تقنيات قريبة من معايير الطيران وتتميز بمتانة أعلى وعتبة درجة حرارة أعلى من 120 درجة مئوية ووزن أقل مقارنة بالمكونات الأصلية.',
+            'become_partner': 'كن شريكًا في Formacar Design!',
+            'about_formacar_design': 'Formacar Design هو استوديو لتصميم السيارات. يضمن إنتاجنا الخاص والتحكم الدقيق في كل مرحلة من مراحل العمل (بدءًا من إنشاء المفهوم والنموذج الأولي وحتى تطوير وتركيب المجموعة) الجودة العالية بشكل استثنائي لمشاريعنا.',
+            'gcc_partners': "نحن نعمل على توسيع تواجدنا في منطقة GCC ونبحث بنشاط عن شركاء وأشخاص ذوي تفكير مماثل لتطوير أعمالنا.",
+            'join_our_team': "إذا كنتم تحلمون بفتح مشروع أعمال ناجح وتطويره في مجال صناعة السيارات، انضم إلى فريقنا!",
+            'formacar_changes': 'Formacar – يغير كل شيء!',
+            'formacar_catalog': 'فهرس',
+            'formacar_invest_desk': '(إتفيست – ديك): عرض تقديمي مفصل مصمم للتحليل المتعمق للأعمال.',
+            'formacar_get_details': 'لمزيد من المعلومات',
+            'formacar_name': 'الاسم',
+            'formacar_purpose_inquiry': 'الغرض من الطلب',
+            'business_partnership': 'العمل أو الشراكة',
+            'formacar_investments': 'الاستثمارات',
+            'formacar_phone': 'رقم الهاتف',
+            'formacar_email': 'E-mail',
+            'formacar_message': 'رسالة',
+            'formacar_send': 'إرسال',
+            'formacar_captcha': 'هذا الموقع محمي بواسطة reCAPTCHA ويتم تطبيق سياسة خصوصية Google وشروط خدمة Google.',
+            'site_captcha': 'هذا الموقع محمي بواسطة reCAPTCHA وGoogle.',
+            'and': 'و',
+            'privacy_policy': 'سياسة الخصوصية',
+            'terms_service': 'شروط الاستخدام',
+            'button_apply': 'يتقدم',
+            'application_sent': 'لقد تم إرسال طلبكم. سنتواصل معكم قريبا.'
+        }
+    }
+
     let not_selected_language_switch_box = document.querySelector('.language_switch__box.not_selected');
     let selected_language_switch_box     = document.querySelector('.language_switch__box.selected');
 
@@ -36,17 +118,6 @@
         change_language_switch_location(selected_language_switch_box, parent_selector);
     }
 
-    let arrLang = {
-        'en': {
-            'hero_formacar_design': 'Formacar Design is a studio dedicated to premium vehicle design and styling.',
-            'hero_the_high': 'The high standards and the individual approach adopted by Formacar offer a fresh perspective on the mundane and let you discover new aspects of automotive design finesse.',
-        },
-        'ar': {
-            'hero_formacar_design': 'هو استوديو متميز لتصميم  السيارات الفاخرة Formacar Design',
-            'hero_the_high': 'تسمح المعايير العالية والنهج المميز للعلامة التجارية Formacar  بالنظر إلى المألوف بشكل مختلف وفتح جوانب جديدة للتميز في تصميم السيارات',
-        }
-    }
-
     function change_language_switch_location(selected_language_switch_box, parent_selector) {
         let selected_language    = selected_language_switch_box.querySelector('.text').id;
         let logo_selector        = document.querySelectorAll(".screen__head");
@@ -80,7 +151,7 @@
                 let tag_bdo   = document.createElement('bdo'); */
                 /* let translate = tag_bdo.textContent = arrLang['ar'][element.getAttribute('key')];
                 element.appendChild(translate); */
-                element.textContent = arrLang['ar'][element.getAttribute('key')];
+                element.textContent = arr_lang['ar'][element.getAttribute('key')];
             });
             
         } else {
@@ -103,7 +174,7 @@
             screen_info.forEach   (selector => selector.style = "text-align: left; padding-right: 65px;");
             
             translate_selector.forEach(element => {
-                element.textContent = arrLang['en'][element.getAttribute('key')];
+                element.textContent = arr_lang['en'][element.getAttribute('key')];
             });
         }
     }
